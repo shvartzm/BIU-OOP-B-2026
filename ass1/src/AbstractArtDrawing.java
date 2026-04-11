d5b197bf31e70bcf7f54949e052e36e622070545
Maorsh <69312417+shvartzm@users.noreply.github.com>
2026-04-11 15:05:20 +0300

    dd

--- ass1/src/AbstractArtDrawing.java
+++ ass1/src/AbstractArtDrawing.java
@@ -17,14 +17,49 @@ public class AbstractArtDrawing {
             int y1 = rnd.nextInt(600) +1 ;
             int y2 = rnd.nextInt(600) +1 ;
             Line l1 = new Line(new Point(x1, y1), new Point(x2, y2));
-            lines[i] = l1;
-            d.setColor(Color.BLACK);
-            d.drawLine(x1, y1, x2, y2);
-            d.setColor(Color.BLUE);
+            this.lines[i] = l1;
+            paintLine(d,x1, y1, x2, y2,Color.BLACK);
             Point middlePoint = l1.middle();
-            d.drawCircle((int)middlePoint.getX(), (int)middlePoint.getY(), 3);
+            paintCircle(d,middlePoint.getX(),middlePoint.getY(), 3, Color.BLUE);
         }
+        drawIntersectionPoints(d);
         gui.show(d);
+    }
+
+    private void drawIntersectionPoints(DrawSurface d){
+        for(int i = 0; i <= lines.length -3; i++){
+            for (int j = i + 1; j <= lines.length - 2; j++){
+                for (int k = j + 1; k < lines.length; k++){
+                    Point p1 = lines[i].intersectionWith(lines[j]);
+                    Point p2 = lines[i].intersectionWith(lines[k]);
+                    Point p3 = lines[j].intersectionWith(lines[k]);
+                    if (p1 != null){
+                        paintCircle(d, p1.getX(), p1.getY(), 3, Color.RED);
+                    }
+                    if(p2 != null){
+                        paintCircle(d, p2.getX(), p2.getY(), 3, Color.RED);
+                    }
+                    if(p3 != null){
+                        paintCircle(d, p3.getX(), p3.getY(), 3, Color.RED);
+                    }
+                    if (p1 != null && p2 != null && p3 != null && lines[i].isIntersecting(lines[k], lines[j])){
+                        paintLine(d, p1.getX(), p1.getY(), p2.getX(), p2.getY(), Color.GREEN);
+                        paintLine(d, p1.getX(), p1.getY(), p3.getX(), p3.getY(), Color.GREEN);
+                        paintLine(d, p3.getX(), p3.getY(), p2.getX(), p2.getY(), Color.GREEN);
+                    }
+                    
+
+                }
+            }
+        }
+    }
+    private void paintCircle(DrawSurface d,double x, double y, int r, Color argColor){
+        d.setColor(argColor);
+        d.fillCircle((int)x,(int) y, r);
+    }
+    private void paintLine(DrawSurface d,double x1, double y1,double x2, double y2, Color argColor){
+        d.setColor(argColor);
+        d.drawLine((int)x1, (int)y1, (int)x2,(int) y2);
     }
     public static void main(String[] args) {
      AbstractArtDrawing example = new AbstractArtDrawing();
